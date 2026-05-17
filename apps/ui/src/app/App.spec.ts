dc7141cc52fd82aa4dc42d78834ba03a0fef2d92
jow <johannes.witt1988@googlemail.com>
2026-05-17 17:27:53 +0200

    feat(ui): cleanup autogenerated vues

--- apps/ui/src/app/App.spec.ts
+++ apps/ui/src/app/App.spec.ts
@@ -1,12 +1,13 @@
-import { describe, it, expect } from 'vitest';
-import router from '../router';
 import { mount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+
+import router from '../router';
 import App from './App.vue';
 
 describe('App', () => {
   it('renders properly', async () => {
     const wrapper = mount(App, { global: { plugins: [router] } });
     await router.isReady();
-    expect(wrapper.text()).toContain('Welcome ui 👋');
+    expect(wrapper.text()).toContain('BotFriends Chat');
   });
 });
